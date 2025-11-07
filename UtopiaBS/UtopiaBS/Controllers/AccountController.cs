f0cc762263bf71d7f6018c7d98226b77a1612d82
iTsJOSEcr <Josedanielhernandezu2@gmail.com>
2025-11-06 19:53:03 -0600

    Modulo de usuarios y roles listo

---
+++ UtopiaBS/UtopiaBS/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using UtopiaBS.Data;
+using UtopiaBS.Models;
+
+namespace UtopiaBS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<UsuarioDA> _userManager;
+
+        public AccountController()
+        {
+            _userManager = new UserManager<UsuarioDA>(new UserStore<UsuarioDA>(new ApplicationDbContext()));
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login() => View();
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.FindAsync(model.Email, model.Password);
+            if (user != null)
+            {
+                var identity = await _userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = false }, identity);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Usuario o contraseña incorrectos.");
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register() => View();
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new UsuarioDA { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                // 👇 Asignar automáticamente el rol "Cliente"
+                await _userManager.AddToRoleAsync(user.Id, "Cliente");
+
+                var identity = await _userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = false }, identity);
+                return RedirectToAction("Index", "Home");
+            }
+
+            AddErrors(result);
+            return View(model);
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
