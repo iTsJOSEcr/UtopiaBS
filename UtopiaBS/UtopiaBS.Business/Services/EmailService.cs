27effd4d7b9bb411363c33c1ab54ab056b7dce80
Alieras <alisseras04@gmail.com>
2025-12-02 07:48:48 -0600

    Correcion Email Password

--- UtopiaBS/UtopiaBS.Business/Services/EmailService.cs
+++ UtopiaBS/UtopiaBS.Business/Services/EmailService.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace UtopiaBS.Business.Services
@@ -13,8 +10,16 @@ namespace UtopiaBS.Business.Services
     {
         public static async Task EnviarCorreoAsync(string para, string asunto, string cuerpoHtml)
         {
+            // El correo viene del web.config (esto está bien)
             string correoSistema = ConfigurationManager.AppSettings["CorreoSistema"];
-            string clave = ConfigurationManager.AppSettings["ClaveCorreo"];
+
+            // ✅ La clave ahora viene de la VARIABLE DE ENTORNO (NO del web.config)
+            string clave = Environment.GetEnvironmentVariable("UTOPIA_SMTP_PASS");
+
+            if (string.IsNullOrEmpty(clave))
+            {
+                throw new Exception("No se encontró la variable de entorno UTOPIA_SMTP_PASS.");
+            }
 
             var mensaje = new MailMessage(correoSistema, para, asunto, cuerpoHtml);
             mensaje.IsBodyHtml = true;
